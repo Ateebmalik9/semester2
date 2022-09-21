cacbca19d3365e59c667e84da1e6461cb3dda5cd
duaa234 <107420796+duaa234@users.noreply.github.com>
2022-09-21 22:57:54 +0530

    Create EvenOdd.c

---
+++ EvenOdd.c
@@ -0,0 +1,15 @@
+//Determine whether a number os even or odd
+#include<stdio.h>
+int main()
+{
+int n;
+	printf("Please enter a number_ ");
+	scanf("%d",&n);
+	if (n%2==0)
+	printf("Value is even");
+	else
+	printf("Value is odd");
+
+
+return 0;
+}
