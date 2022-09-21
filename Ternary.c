449a4902328f0df23ed75227c6c1f66910c8653b
duaa234 <107420796+duaa234@users.noreply.github.com>
2022-09-21 22:59:23 +0530

    Create Ternary.c

---
+++ Ternary.c
@@ -0,0 +1,11 @@
+// Max. number using Ternary Operator
+#include<stdio.h>
+int main()
+{ 
+int a,b,z;
+printf("enter value");
+scanf("%d %d",&a,&b);
+z=a>b? a:b;
+printf("the greatest interger is %d",z);
+return 0;
+}
