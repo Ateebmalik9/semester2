b3fe4ec92b8ccfffb4d77b4791daf9a09a5837c6
duaa234 <107420796+duaa234@users.noreply.github.com>
2022-09-22 05:58:34 +0530

    Create Asterisk_Square.c

---
+++ Asterisk_Square.c
@@ -0,0 +1,19 @@
+// square
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for (i=1;i<=6;i++)
+    {
+        for(j=1;j<=6;j++)
+        {
+            printf("* ");
+
+        }
+
+            printf("\n");
+    }
+
+}
+
+
